90492ce5c54784c9117a1dc8e180f0b178417208
Your Name <acboulet@outlook.com>
2021-05-13 16:11:21 -0600

    Day6 challenge 1 is solved with correct answer

--- d7/day7.py
+++ d7/day7.py
@@ -31,6 +31,18 @@ def process_1(file_name):
     return all_bags
 
 def dict_tree(list_bags):
+    """
+    Purpose:
+        Take a list of bags, and process into a dictionary
+    Pre:
+        :param list_bags: An arrayed list where each item is described:
+            [0] = bag that contains all other bags
+            [1:] = bags that can be held in [0]
+    Return:
+        Dictionary where key is bag name, and value is a list bags:
+            [0] : number of this type of bag that can be stored
+            [1] : bag that can be stored
+    """
     dict_tree = {}
     for bag in list_bags:
         dict_tree[bag[0]] = []
@@ -41,9 +53,51 @@ def dict_tree(list_bags):
                 dict_tree[bag[0]].append([0, "none"])
     return dict_tree
 
+def find_path(dict_bag):
+    """
+    Purpose:
+        Find how many different types of bags can store a "shiny gold" bag either directly, or indirectly
+    Pre:
+        :param dict_bag: Return value from dict_tree()
+    Return:
+        The number of bags that can store "shiny gold" 
+    """
+    bags = set() # Stores all bags that can store a "shiny gold"
+    list_gold = list_of_gold(dict_bag, "shiny gold") # All bags that can directly store a "shiny gold"
+    # Use a python list as a queue
+    # Remove the first bag from the list, add it "bags", and find any bags that can store this first bag
+    # Then extend the list with those bags, and use them as future search terms
+    while len(list_gold) > 0:
+        bag = list_gold[0]
+        bags.add(bag)
+        list_gold.remove(bag)
+        list_gold.extend( list_of_gold(dict_bag, bag) ) 
+    return len(bags)
+
+
+    
+def list_of_gold(dict_bag, search_bag):
+    """
+    Purpose:
+        Find which bags can store a specific bag
+    Pre:
+        :param dict_bag: Output from dict_tree
+        :param search_bag: String rep bag name to search for
+    Return:
+        List of all bags that can store search_bag
+    """
+    list_gold = []
+    for bag in dict_bag:
+        for stored in dict_bag[bag]:
+            if search_bag in stored:
+                list_gold.append(bag)
+    return list_gold
+
 
 
 if __name__ == "__main__":
-    bag_list = process_1("d7/day7_example.txt")
+    bag_list = process_1("d7/day7_input.txt")
     org_list = dict_tree(bag_list)
-    print(org_list)
+    gold_bags = find_path(org_list)
+    print(gold_bags)
+
